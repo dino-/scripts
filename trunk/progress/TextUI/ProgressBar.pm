d0e4243bfd1e113a0814e7c9f2b4a2c3bd457d9b
dmorelli <unknown>
2005-04-18 03:29:12 +0000

    [svn-to-darcs @ 84]
    Cleaned up this code and made better tests.

--- trunk/progress/TextUI/ProgressBar.pm
+++ trunk/progress/TextUI/ProgressBar.pm
@@ -3,8 +3,8 @@ package TextUI::ProgressBar;
 #--------------------------------------------------------------------------
 # $RCSfile$
 #--------------------------------------------------------------------------
-# $Revision: 36 $
-# $Date: 2004-07-08 12:00:50 -0400 (Thu, 08 Jul 2004) $
+# $Revision: 84 $
+# $Date: 2005-04-17 23:29:12 -0400 (Sun, 17 Apr 2005) $
 # $Author: dmorelli $
 #
 # Class to draw a text progress bar.
@@ -13,30 +13,39 @@ package TextUI::ProgressBar;
 
 #--------------------------------------------------------------------------
 # Construct a new progress bar.
-# Pass in the max value you will be counting up to.
-sub new($$)
-{
+# args:
+#   class
+#   maxValue  max actual value
+sub new {
 	my $this = (bless {}, shift);
-	$this{actualMax} = shift;
-	$this{actualCurr} = 0;
-	$this{displayMax} = 20;
-	$this{percentFormat} = "%#3d";
+	$this->init(shift);
 
 	return $this;
 }
 
 #--------------------------------------------------------------------------
+# Initialize this instance
+# args:
+#   instance
+#   maxValue  max actual value
+sub init {
+	my $this = shift;
+
+	$this{actualMax} = shift;
+	$this->setDisplayWidth(20);
+	$this->setPercentFormat('%#3d');
+}
+
+#--------------------------------------------------------------------------
 # Set the maximum width for the [==> ] portion of the progress bar.
-sub setDisplayWidth($$)
-{
+sub setDisplayWidth {
 	my $this = shift;
 	$this{displayMax} = shift;
 }
 
 #--------------------------------------------------------------------------
 # Set the printf format string for the percentage.
-sub setPercentFormat($$)
-{
+sub setPercentFormat {
 	my $this = shift;
 	$this{percentFormat} = shift;
 }
@@ -44,38 +53,24 @@ sub setPercentFormat($$)
 #--------------------------------------------------------------------------
 # Redraw the display.
 # Pass in current so-far value.
-sub update($$)
-{
+sub update {
 	my ($this, $value) = @_;
 
-	$this{actualCurr} = $value;
-	my $displayCurr = ($this{actualCurr} / $this{actualMax}) *
+	my $displayCurr = ($value / $this{actualMax}) *
 		$this{displayMax};
 
 	# We need autoflush on but don't mess with the global value.
 	local $| = 1;
 
-	print "\r[";
-	my $i = 0;
-	while($i < $displayCurr - 1)
-	{
-		print "=";
-		$i++;
-	}
-	print ">";
-	$i++;
-	while($i < $this{displayMax})
-	{
-		print " ";
-		$i++;
-	}
+	my $dcAsInt = int($displayCurr);
+	print ("\r[" . ('=' x $dcAsInt) . ">" .
+		(' ' x ($this{displayMax} - $dcAsInt)));
 
-	my $percent = ($this{actualCurr} / $this{actualMax}) * 100;
-
-	printf("| $this{percentFormat}%%]", $percent);
+	printf("| $this{percentFormat}%%]", 
+		($value / $this{actualMax}) * 100);
 
 	# FIXME debugging
-	#print "  $this{actualCurr}  $displayCurr";
+	#print "  $value  $displayCurr";
 }
 
-1;  # Module initialized successfully.
+1;
