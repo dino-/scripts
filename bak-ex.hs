fea5ce2f990bb759f35e8ce34e9ab548daf62d6b
Dino Morelli <dino@ui3.info>
2008-01-12 02:29:03 +0000

    Added and fixed comments

--- bak-ex.hs
+++ bak-ex.hs
@@ -5,6 +5,7 @@ import Data.List ( intersperse )
 import System.Process ( runCommand, waitForProcess )
 
 
+-- Trailing / on src dir means DON'T include the last dir from src
 srcs =
    [ ("/boot", "")
    , ("/etc", "")
@@ -22,12 +23,13 @@ commonSwitches = "-av -R --delete"
 destPath = "dinoatt@ui3.info:/var/local/backup/agave"
 
 
--- This one outputs all errors to stdout, but otherwise lets the
+-- This one redirects all errors to stdout, but otherwise lets the
 -- invoker deal with logging it or not
 output = "2>&1"
 
---output = "2>&1 | tee -a /var/tmp/dmorelli-bu.log"
---output = "2>&1 > /var/tmp/dmorelli-bu.log"
+-- Some other possible output postfixes:
+--output = "2>&1 | tee -a /var/tmp/bak-foo.log"
+--output = "2>&1 > /var/log/bak-foo.log"
 
 
 delimWithSpace :: [String] -> String
