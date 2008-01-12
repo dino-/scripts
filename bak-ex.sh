ee38ba42970119efcd6682e450479aa61616d6cd
Dino Morelli <dino@ui3.info>
2008-01-12 02:24:34 +0000

    Added sh backup script

---
+++ bak-ex.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+prefix="rsync -av -R --delete"
+dest="user@host.blah:/top/level/remote/backup/dir"
+postfix="2>&1"
+
+date
+
+# Trailing / on src dir means DON'T include the last dir from src
+$prefix /var/spool/cron $dest $postfix
+$prefix /var/mail $dest $postfix
+$prefix /var/log $dest $postfix
+$prefix /root $dest $postfix
+$prefix --exclude .mozilla/firefox/*/Cache --delete-excluded /home $dest $postfix
+$prefix /etc $dest $postfix
+$prefix /boot $dest $postfix
+
+date
