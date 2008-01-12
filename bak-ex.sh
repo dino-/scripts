0d83c2ab3c8d2c16017a72c42f0c8d71e9615c42
Dino Morelli <dino@ui3.info>
2008-01-12 17:56:04 +0000

    Modified order of the backup commands

--- bak-ex.sh
+++ bak-ex.sh
@@ -8,12 +8,13 @@ output="2>&1"
 date
 
 # Trailing / on src dir means DON'T include the last dir from src
-$prefix /var/spool/cron $destPath $output
-$prefix /var/mail $destPath $output
-$prefix /var/log $destPath $output
-$prefix /root $destPath $output
-$prefix --exclude .mozilla/firefox/*/Cache --delete-excluded /home $destPath $output
-$prefix /etc $destPath $output
 $prefix /boot $destPath $output
+$prefix /etc $destPath $output
+$prefix --exclude .mozilla/firefox/*/Cache --delete-excluded /home $destPath $output
+$prefix /root $destPath $output
+$prefix /var/lib/dpkg $destPath $output
+$prefix /var/log $destPath $output
+$prefix /var/mail $destPath $output
+$prefix /var/spool/cron $destPath $output
 
 date
