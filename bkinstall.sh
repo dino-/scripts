56e2804d5f03b2c0b32ba60d9fee1c6c119eb1fa
Dino Morelli <dino@ui3.info>
2009-04-01 23:12:26 +0000

    Updated book installation script with latest info on my reader's mount point, etc

--- bkinstall.sh
+++ bkinstall.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
 
-mp="/media/prs505"
+mp="/media/prs505/int"
 src="$HOME/temp/books/install"
 dest="$mp/database/media/books"
 
 mount $mp
 echo "mounted reader at $mp"
 
-cp -v $src/* $dest
-
-rm -fv $src/*
+cp -v $* $dest
 
 umount $mp
 echo "unmounted reader"
