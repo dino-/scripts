74c1d46f65461de6065f722f08f54330d8867985
Dino Morelli <dino@ui3.info>
2012-01-03 18:32:28 +0000

    Changed formatting

--- getcoretemp.sh
+++ getcoretemp.sh
@@ -7,4 +7,4 @@ dir="/sys/devices/platform/coretemp.0"
 core0=$(cat $dir/temp2_input)
 core1=$(cat $dir/temp3_input)
 
-echo "TempC: $(expr $core0 / 1000) $(expr $core1 / 1000)"
+echo "Cpu temp: $(expr $core0 / 1000)C $(expr $core1 / 1000)C"
