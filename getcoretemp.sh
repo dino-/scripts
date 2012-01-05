681a3a394c1ce2707f6c9f00ead54e153536d657
Dino Morelli <dino@ui3.info>
2012-01-05 15:19:08 +0000

    Made output even sparer

--- getcoretemp.sh
+++ getcoretemp.sh
@@ -7,4 +7,4 @@ dir="/sys/devices/platform/coretemp.0"
 core0=$(cat $dir/temp2_input)
 core1=$(cat $dir/temp3_input)
 
-echo "Cpu temp: $(expr $core0 / 1000)C $(expr $core1 / 1000)C"
+echo "$(expr $core0 / 1000)C $(expr $core1 / 1000)C"
