e9d6321c65b1be631fa1d7de0425b4ee39b93326
Dino Morelli <dino@ui3.info>
2012-07-12 13:42:58 +0000

    Updated script to handle the temps of 4 cores

--- getcoretemp.sh
+++ getcoretemp.sh
@@ -6,5 +6,7 @@ dir="/sys/devices/platform/coretemp.0"
 
 core0=$(cat $dir/temp2_input)
 core1=$(cat $dir/temp3_input)
+core2=$(cat $dir/temp4_input)
+core3=$(cat $dir/temp5_input)
 
-echo "$(expr $core0 / 1000)C $(expr $core1 / 1000)C"
+echo "$(expr $core0 / 1000)C $(expr $core1 / 1000)C $(expr $core2 / 1000)C $(expr $core3 / 1000)C"
