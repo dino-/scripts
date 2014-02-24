bab1732e9ddf99ad192db7d4a1b78df6a84a91a6
Dino Morelli <dino@ui3.info>
2014-02-24 19:11:39 +0000

    Added new script to parse and format dates
    This replaces the informal time.hs script I've been using for many
    years. This one can parse more varied input, handles milliseconds
    and is well-documented.

---
+++ epochconv.hs
@@ -0,0 +1,135 @@
+#! /usr/bin/env runhaskell
+
+import Control.Monad ( mplus )
+import Data.Char ( isDigit )
+import Data.List ( intercalate )
+import Data.Time
+   ( FormatTime, ParseTime, UTCTime
+   , formatTime, getCurrentTime, parseTime, utcToLocalZonedTime
+   )
+import Data.Time.Clock.POSIX
+   ( posixSecondsToUTCTime, utcTimeToPOSIXSeconds )
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure, exitSuccess )
+import System.Locale ( defaultTimeLocale )
+import Text.Printf ( printf )
+
+
+{- Some important points:
+
+   POSIXTime is basically this: secs-since-epoch.picoseconds
+   as a fractional number
+
+   By using (fromIntegral . truncate) on it, you can strip off the picos
+   getting what we all know as epoch time. Or you can use parseTime "%s"
+
+   But UTCTime is the low-level format that everything uses in the 
+   Haskell APIs
+
+   Functions for converting between the two are in
+   Data.Time.Clock.POSIX
+-}
+
+
+main :: IO ()
+main = getArgs >>= parseInput >>= output
+
+
+parseInput :: [String] -> IO (Either String UTCTime)
+parseInput []         = Right `fmap` getCurrentTime
+parseInput ["-h"]     = Left `fmap` usage
+parseInput ["--help"] = Left `fmap` usage
+parseInput as         = return . parseInput' $ as
+
+
+usage :: IO String
+usage = do
+   appName <- getProgName
+   return $ unlines
+      [ "Show a given date (or the current date) in a variety of formats"
+      , ""
+      , "Usage: " ++ appName ++ " [OPTION] [DATE]"
+      , ""
+      , "Options:"
+      , "  -e          Input is epoch (seconds since 1970-01-01)"
+      , "  -m          Input is milliseconds (since 1970-01-01)"
+      , "  -f          Input is a human-readable date"
+      , "  -h, --help  This usage information"
+      , ""
+      , "If no date is given, the current date/time will be used."
+      , "The -e and -f options are mostly unnecessary. -m is needed to"
+      , "disambiguate between epoch and milliseconds."
+      , ""                                                 
+      , "Output will be the date/time in a variety of formats, both localized"                                                 
+      , "and UTC, as well as epoch and milliseconds."
+      , ""                                                 
+      , "Dino Morelli <dino@ui3.info>"                     
+      ]
+
+
+parseInput' :: [String] -> Either String UTCTime
+parseInput' ("-e" : epochString : []) = strToUTCTime 1 epochString
+parseInput' ("-m" : milliString : []) = strToUTCTime 1000 milliString
+parseInput' ("-f" : as)               = parseDateString $ joinArgs as
+parseInput' as
+   | any (not . isDigit) $ joinArgs as = parseInput' $ "-f" : as
+   | otherwise = strToUTCTime 1 (joinArgs as)
+
+
+strToUTCTime :: Double -> String -> Either String UTCTime
+strToUTCTime divisor = Right . posixSecondsToUTCTime . realToFrac
+   . (/ divisor) . read
+
+
+joinArgs :: [String] -> String
+joinArgs = intercalate " "
+
+
+parseDateString :: String -> Either String UTCTime
+parseDateString s = maybe (Left $ "Unable to parse " ++ s)
+   (Right . id) $ foldl mplus Nothing $ map ($ s) parsers
+
+
+rfc5322Date, iso1601Offset, iso1601Zulu :: String
+
+-- Fri, 21 Nov 1997 10:55:06 -0500
+rfc5322Date = "%a, %d %b %Y %T %z"
+-- 1997-11-21T10:55:06-0500
+iso1601Offset = "%FT%T%z"
+-- 1997-11-21T15:55:06Z
+iso1601Zulu = "%FT%TZ"
+
+
+parsers :: ParseTime t => [String -> Maybe t]
+parsers = map (parseTime defaultTimeLocale)
+   [ "%c"
+   , rfc5322Date
+   , iso1601Offset
+   , iso1601Zulu
+   , "%F"
+   ]
+
+
+output :: Either String UTCTime -> IO ()
+
+output (Left errMsg) = do
+   putStrLn errMsg
+   exitFailure
+
+output (Right ut) = do
+   local <- utcToLocalZonedTime ut
+
+   putStrLn $ "     RFC5322: " ++ fmt rfc5322Date local
+
+   printf "\n %s ISO1601: %s\n" (fmt "%Z" local) (fmt iso1601Offset local)
+   putStrLn $ " UTC ISO1601: " ++ fmt iso1601Zulu ut
+
+   putStrLn $ "\n       epoch: " ++ fmt "%s" ut
+   putStrLn $ "milliseconds: " ++ (show . fromIntegral . truncate
+      . (* 1000) . realToFrac . utcTimeToPOSIXSeconds $ ut)
+
+   exitSuccess
+
+   where
+      fmt :: FormatTime t => String -> t -> String
+      fmt = formatTime defaultTimeLocale
