22a9fe96bdf1c0999b921e6cb4a8aa7619cca0f2
Dino Morelli <dino@ui3.info>
2015-09-24 16:59:57 +0000

    Removed Dump Truck specific command lines
    Use of Golden Frog's Dump Truck service for my shared cloud storage
    has been abandoned because of a combination of a recent, severe
    outage there, my dissatisfaction with pretty much every one of
    these types of services and finally the general bad behavior of
    WebDAV/davfs2.

--- sync-cloud.sh
+++ sync-cloud.sh
@@ -5,8 +5,3 @@
 #flock -n /home/dino/var/run/lock/unison.lock unison $* -ui text cloud
 # Having problems with idiot Unison and idiot OCaml. Version incompatibility between what's on Arch Linux and what's on Debian. Falling back to mounting the remote storage locally so it's ONE copy of Unison doing everything
 /home/dino/bin/withmount.hs /media/cloud flock -n /home/dino/var/run/lock/unison-cloud.lock unison $* -ui text cloud
-
-
-# These are from the now-abandoned Dump Truck service
-#/home/dino/bin/withmount.hs /media/dav/dumptruck flock -n /home/dino/var/run/lock/unison.lock unison $* -ui text cloud
-#/home/dino/bin/withmount.hs /media/dav/dumptruck flock -n /home/dino/var/run/lock/unison.lock unison $* -ui text cloud 2>/home/dino/var/log/unison$(date +"%Y%m%d-%H%M").err
