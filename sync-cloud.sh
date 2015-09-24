9d4af456c0afbae193b7f7b7b50c8856d06ce6fc
Dino Morelli <dino@ui3.info>
2015-09-24 17:08:05 +0000

    Added some usage notes

--- sync-cloud.sh
+++ sync-cloud.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-# Use the additional -silent switch when running from cron
+# Usage notes:
+# Add the -silent switch when running from cron to suppress all
+#   output unless there is a problem
+# Add the -batch=false switch to manually resolve conflicts
 
 #flock -n /home/dino/var/run/lock/unison.lock unison $* -ui text cloud
 # Having problems with idiot Unison and idiot OCaml. Version incompatibility between what's on Arch Linux and what's on Debian. Falling back to mounting the remote storage locally so it's ONE copy of Unison doing everything
