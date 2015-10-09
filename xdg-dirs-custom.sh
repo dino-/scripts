ecddbb403889559944351df16f71363bdb0579ab
Dino Morelli <dino@ui3.info>
2015-10-09 15:43:25 +0000

    Changed some of these directories

--- xdg-dirs-custom.sh
+++ xdg-dirs-custom.sh
@@ -3,8 +3,12 @@
 # Do this when your system is new
 
 xdg-user-dirs-update --set DESKTOP ~
-xdg-user-dirs-update --set PICTURES ~
-xdg-user-dirs-update --set VIDEOS ~
+
+mkdir -p ~/pictures/misc
+xdg-user-dirs-update --set PICTURES ~/pictures/misc
+
+mkdir -p ~/pictures/video
+xdg-user-dirs-update --set VIDEOS ~/pictures/video
 
 mkdir -p ~/doc
 xdg-user-dirs-update --set DOCUMENTS ~/doc
@@ -14,7 +18,9 @@ xdg-user-dirs-update --set TEMPLATES ~/doc/templates
 
 mkdir -p ~/tmp
 xdg-user-dirs-update --set DOWNLOAD ~/tmp
-xdg-user-dirs-update --set MUSIC ~/tmp
+
+mkdir -p ~/tmp/music
+xdg-user-dirs-update --set MUSIC ~/tmp/music
 
 mkdir -p ~/tmp/share
 xdg-user-dirs-update --set PUBLICSHARE ~/tmp/share
