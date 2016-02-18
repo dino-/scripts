9789a7f7264fb0de13341968ed68a2b5a4005fa6
Dino Morelli <dino@ui3.info>
2016-02-18 18:05:08 +0000

    Added a bash script to illustrate args parsing

---
+++ args-example.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+
+basename=$(basename $0)
+
+
+function usage {
+   cat <<USAGE
+$basename - Argument parsing demo script
+
+usage:
+   $basename [OPTIONS] ITEM1 ITEM2 ...
+
+options:
+   -a, --arg-a       An argument named a. Default false
+   -b, --arg-b INT   An argument named b
+   -h, --help        This help information
+
+v1.0  2016-02-17  Dino Morelli <dino@ui3.info>
+
+USAGE
+}
+
+
+# arg parsing
+
+getoptResults=$(getopt -o ab:h --long arg-a,arg-b:,help -n $basename -- "$@")
+
+if [ $? != 0 ]; then usage; exit 1; fi
+
+# Note the quotes around '$getoptResults': they are essential!
+eval set -- "$getoptResults"
+
+while true ; do
+	case "$1" in
+      -a|--arg-a) optA=1; shift;;
+      -b|--arg-b) optB="$2"; shift 2;;
+      -h|--help) optHelp=1; shift;;
+		--) shift; break;;
+	esac
+done
+
+echo "detail of arguments"
+echo "optA: $optA"
+echo "optB: $optB"
+echo "optHelp: $optHelp"
+echo "number of remaining parameters: $#"
+echo "remaining parameters: $@"
+echo
+
+if [ "$optHelp" = 1 ]; then usage; exit 0; fi
+
+if [ $# -lt 2 ]
+then
+   echo "Incorrect number of ITEMs"
+   usage
+   exit 1
+fi
