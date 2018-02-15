550c9deaf0fe7c2c79cb9b50a2c9acd002004ce2
Dino Morelli <dino@ui3.info>
2018-02-15 10:42:09 -0500

    Added new script for linking new photos into my archives

---
+++ mv-photos.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+
+# Everything we do from here is relative to this directory
+cd $HOME/doc/cloud/mobile/phone/camera
+
+# We need to perform this command more than once
+findcmd="find . -name '*.jpg' -links 1 -print"
+
+# If we have no files, get out of here now
+test -z "$(eval $findcmd -quit)" && exit 0
+
+# We're really doing something, log the date
+date +"%F %T"
+
+# Use the find command to really rename the files this time
+eval $findcmd | xargs photoname -p $HOME/pictures/photos
+
+# A blank line to separate this from the next log entries
+echo
