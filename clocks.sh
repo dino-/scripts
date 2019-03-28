fb40d2750a29489850fcc8abba5262c99c71f00e
Dino Morelli <dino@ui3.info>
2019-03-28 10:08:28 -0400

    Cleaned up this script and added more colors
    The date command lines are now more uniform. The time format is now in a
    variable for re-use. I added a lot more colors and renamed all of the
    color variables. Also added some helpful comments for locating valid
    values for the TZ environment variable.

--- clocks.sh
+++ clocks.sh
@@ -1,18 +1,30 @@
 #! /bin/bash
 
-cyan="\e[36m"
-green="\e[32m"
-yellow="\e[33m"
-endColor="\e[0m"
+# For a list of files for TZ, see /usr/share/zoneinfo
 
+txtreset="\e[0m"
+
+txtred="\e[0;31m"  # Difficult to see
+bldred="\e[1;31m"
+txtgreen="\e[0;32m"
+txtyellow="\e[0;33m"
+txtblue="\e[0;34m"  # Difficult to see
+bldblue="\e[1;34m"  # Difficult to see
+txtpurple="\e[0;35m"  # Difficult to see
+bldpurple="\e[1;35m"
+txtcyan="\e[0;36m"
+txtwhite="\e[0;37m"
+
+timeFormat="%F %T %Z"
 
 while true
 do
    clear
 
-   echo -en "$green"; date -u +"%F %T %Z"; echo -en "$endColor"
-   echo -en "$yellow"; date +"%F %T %Z"; echo -en "$endColor"
-   echo -en "$cyan"; TZ='America/Los_Angeles' date +"%F %T %Z"; echo -en "$endColor"
+   echo -en "$txtyellow"; TZ='America/Los_Angeles' date +"$timeFormat"; echo -en "$txtreset"
+   echo -en "$txtcyan"; TZ='America/New_York' date +"$timeFormat"; echo -en "$txtreset"
+   echo -en "$txtgreen"; TZ='UTC' date +"$timeFormat"; echo -en "$txtreset"
+   # echo -en "$bldpurple"; TZ='EET' date +"$timeFormat"; echo -en "$txtreset"
 
    sleep 1
 done
