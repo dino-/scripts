1a90963dee8afb3eeeb98f90875e5b937e777e03
Dino Morelli <dino@ui3.info>
2019-08-18 12:23:47 -0400

    Fixed a variable quoting issue

--- sync-cloud.sh
+++ sync-cloud.sh
@@ -14,4 +14,4 @@ withmount="${specialhome}/dev/scripts/withmount.hs"
 
 #flock -n "${specialhome}/var/run/lock/unison.lock" unison $* -ui text cloud
 # Having problems with idiot Unison and idiot OCaml. Version incompatibility between what's on Arch Linux and what's on Debian. Falling back to mounting the remote storage locally so it's ONE copy of Unison doing everything
-$withmount /media/cloud flock -n "${specialhome}/var/run/lock/unison-cloud.lock" unison $* -ui text cloud
+$withmount /media/cloud flock -n "${specialhome}/var/run/lock/unison-cloud.lock" unison "$@" -ui text cloud
