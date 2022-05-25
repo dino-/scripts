33b2dc4982466a7f1e890183d4f230e26a9cbec3
Dino Morelli <dino@ui3.info>
2022-05-25 15:04:46 -0400

    Added comment behavior
    Also added a color, cleaned up some of the code, added more time zones
    and reassigned color order.

--- clocks.sh
+++ clocks.sh
@@ -7,6 +7,7 @@ colRedNrm="\e[0;31m"  # Difficult to see
 colRedBld="\e[1;31m"
 colGreenNrm="\e[0;32m"
 colYellowNrm="\e[0;33m"
+colYellowBld="\e[1;33m"
 colBlueNrm="\e[0;34m"  # Difficult to see
 colBlueBld="\e[1;34m"  # Difficult to see
 colPurpleNrm="\e[0;35m"  # Difficult to see
@@ -20,10 +21,13 @@ colReset="\e[0m"
 function withColor() {
   color=$1
   zone=$2
+  comment=$3
   # timeFormat="%F %T %Z"
   timeFormat="%F %T %_4Z %z"
 
-  echo -en "$color"; TZ="$zone" date +"$timeFormat $zone"; echo -en "$colReset"
+  echo -en "$color"
+  TZ="$zone" date +"$timeFormat $zone - $comment"
+  echo -en "$colReset"
 }
 
 
@@ -31,14 +35,14 @@ while true
 do
   clear
 
-  withColor "$colYellowNrm" 'America/Los_Angeles'   # PST/PDT
-  withColor "$colBlueBld" 'America/Denver'          # MT
-  withColor "$colCyanNrm" 'America/New_York'        # EST/EDT
+  withColor "$colYellowNrm" 'America/Los_Angeles' 'PST/PDT'
+  withColor "$colBlueBld" 'America/Denver' 'MT'
+  withColor "$colWhiteNrm" 'America/New_York' 'Dino  EST/EDT'
   withColor "$colGreenNrm" 'UTC'
-  withColor "$colPurpleBld" 'Europe/Berlin'         # CET
-  withColor "$colRedBld" 'EET'                      # Eastern European Time (EET)
-  withColor "$colBlueBld" 'Asia/Kolkata'            # All of India (IST)
-  withColor "$colCyanNrm" 'Portugal'                # WET
+  withColor "$colCyanNrm" 'Portugal' 'WET'
+  withColor "$colPurpleBld" 'Europe/Berlin' 'CET'
+  withColor "$colRedBld" 'EET' 'Eastern European Time'
+  withColor "$colYellowBld" 'Asia/Kolkata' 'All of India (IST)'
 
   sleep 1
 done
