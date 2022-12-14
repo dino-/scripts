df3daded134944c925014eab880279c5f134e072
Dino Morelli <dino@ui3.info>
2022-12-14 08:52:46 -0500

    Switched from sshfs to rclone for remote mounting
    Recent changes to ssh exposed some intermittant failures with sshfs.
    Investigation revealed two things about sshfs:
    
    1. sshfs' debugging switches and features either don't work at all or
       hang the connection
    2. sshfs is no longer being maintained
    
    A reliable alternative is rclone, which can do secure mounting.
    
    Also added more safety to this script with an explicit exit if the
    remote mount fails.

--- sync-cloud.sh
+++ sync-cloud.sh
@@ -7,11 +7,32 @@
 #   -force=/home/dino/doc/cloud  To force the local copy to be copied to the remote
 
 
+basename=$(basename "$0")
+
+die () {
+  rc="$1"
+  shift
+  echo "$basename:" "$@" >&2
+  exit "$rc"
+}
+
+
 # This script relies on another tool, and is routinely run from
-# a cron job, with a sketchy env! Be sure we can locate things:
+# a systemd timer or cron job, with a sketchy env! Be sure we can locate
+# things:
 specialhome=/home/dino
-withmount="${specialhome}/dev/scripts/withmount.hs"
+# withmount="${specialhome}/dev/scripts/withmount.hs"
 
 #flock -n "${specialhome}/var/run/lock/unison.lock" unison $* -ui text cloud
 # Having problems with idiot Unison and idiot OCaml. Version incompatibility between what's on Arch Linux and what's on Debian. Falling back to mounting the remote storage locally so it's ONE copy of Unison doing everything
-$withmount /media/cloud flock -n "${specialhome}/var/run/lock/unison-cloud.lock" unison "$@" -ui text cloud
+# $withmount /media/cloud flock -n "${specialhome}/var/run/lock/unison-cloud.lock" unison "$@" -ui text cloud
+
+mountPoint="/media/cloud"
+
+# Note: 'cloud:' here refers to a remote system configured in $HOME/.config/rclone/rclone.conf
+rclone mount --daemon cloud:doc/cloud "$mountPoint" \
+  || die 1 "Can't continue because the rclone mount failed"
+
+flock -n "${specialhome}/var/run/lock/unison-cloud.lock" unison "$@" -ui text cloud
+
+fusermount -u "$mountPoint"
