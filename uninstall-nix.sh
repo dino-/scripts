976510892d94173dbc71b82a8d409c1ee5b9625b
Dino Morelli <dino@ui3.info>
2024-02-22 22:44:22 -0500

    Extensive updates to this nix uninstaller
    This now works for single- and multi-user installations via both the
    official nixos.org installer or the Arch Linux extra/nix package. In the
    latter case, use pacman -R first and use this script to clean up the
    idiot mess left after.

--- uninstall-nix.sh
+++ uninstall-nix.sh
@@ -11,7 +11,7 @@ usage:
   $basename [OPTIONS]
 
 options:
-  --config    Also remove your user config directory
+  --config    Also remove user and root config files
   -h, --help  This help information
 
 Nix is one of those tools that often doesn't install with a distro's proper
@@ -20,10 +20,10 @@ making sure you get all the files and changes it has made. This script will
 scrub a Nix installation from a Linux system.
 
 Note that the --config switch will only remove the config files for the user
-who invoked this script. It may be necessary to track down other users' config
-files by-hand.
+who invoked this script and the root user's HOME. It may be necessary to track
+down other users' config files by-hand.
 
-v1.1  2022-10-23  Dino Morelli <dino@ui3.info>
+v1.2  2024-02-22  Dino Morelli <dino@ui3.info>
 
 USAGE
 )
@@ -71,15 +71,34 @@ fi
 
 set -x
 
-sudo systemctl disable --now nix-daemon.service
-sudo systemctl disable --now nix-daemon.socket
+sudo systemctl stop nix-daemon.service
+sudo systemctl disable nix-daemon.socket nix-daemon.service
+sudo systemctl daemon-reload
+
+# These are left behind by the arch package after pacman -R
+sudo rm /etc/systemd/system/multi-user.target.wants/nix-daemon.service
+sudo rm /etc/systemd/system/sockets.target.wants/nix-daemon.socket
 
 rm -rf "$HOME"/{.nix-*,.cache/nix}
+
+localStateDir="$HOME/.local/state"
+rm -rf "$localStateDir/nix"
+# If the ~/.local/state directory is empty after this it's likely nix made that too, get rid of it
+[ -z "$(ls -A $localStateDir)" ] && rm -rf "$localStateDir"
+
 sudo rm -rf /root/{.nix-channels,.nix-defexpr,.nix-profile,.cache/nix}
-sudo rm -rf /etc/profile.d/nix.sh*
+sudo rm /etc/bash.bashrc.backup-before-nix
+sudo rm /etc/profile.d/nix.sh*
+sudo rm /etc/tmpfiles.d/nix-daemon.conf
 sudo rm -rf /nix
 
+# This is how the official nixos.org installer makes users
 sudo sh -c 'for N in $(seq 32); do userdel "nixbld$N"; done'
+
+# and this is how the arch's extra/nix package makes users (only 10 and 0-padded)
+sudo sh -c 'for N in $(seq 10); do userdel $(printf "nixbld%02d" $N) ; done'
+
+sudo groupdel nixbld
 
 if $optRemoveConfig
 then
@@ -87,6 +106,8 @@ then
   sudo rm -rf /root/.config/nixpkgs
   sudo rm -rf /etc/nix
 fi
+
+set +x
 
 echo "Uninstallation complete."
 echo "This script did not modify your $HOME/.bash_profile or $HOME/.profile, there may be nix-specific things there that you need to take care of."
