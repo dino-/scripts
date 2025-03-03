35aab101f5f0ea9437c62c3104cd60cfc0d4a7fd
Dino Morelli <dino@ui3.info>
2025-03-03 14:23:48 -0500

    Fixed some variable expansion and quoting issues

--- xdg-dirs-custom.sh
+++ xdg-dirs-custom.sh
@@ -20,16 +20,16 @@ function setDir {
    xdg=$1
    dir=$2
 
-   mkdir -p $dir
-   xdg-user-dirs-update --set $xdg $dir
+   mkdir -p "$dir"
+   xdg-user-dirs-update --set "$xdg" "$dir"
 }
 
 
-setDir DESKTOP $HOME/.local/share/desktop
-setDir PICTURES $HOME/pictures/misc
-setDir VIDEOS $HOME/pictures/video
-setDir DOCUMENTS $HOME/doc
-setDir TEMPLATES $HOME/doc/templates
-setDir DOWNLOAD $HOME/tmp
-setDir MUSIC $HOME/sound/music
-setDir PUBLICSHARE $HOME/tmp/share
+setDir DESKTOP "$HOME/.local/share/desktop"
+setDir PICTURES "$HOME/pictures/misc"
+setDir VIDEOS "$HOME/pictures/video"
+setDir DOCUMENTS "$HOME/doc"
+setDir TEMPLATES "$HOME/doc/templates"
+setDir DOWNLOAD "$HOME/tmp"
+setDir MUSIC "$HOME/sound/music"
+setDir PUBLICSHARE "$HOME/tmp/share"
